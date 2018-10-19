ea095c5c3b6f22e75a751200e535894dc3324f63
Kaitlin Newson <kaitlin.newson@utoronto.ca>
2018-10-19 13:37:35 -0400

    add a cookie so french users are not sent back to the french site after the first visit

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -3,6 +3,23 @@ var currentLocation = window.location.href.replace(/\/$/, '');
 var lastPart = currentLocation.substr(currentLocation.lastIndexOf('/') + 1);
 var lang = navigator.language.substr(0,2);
 
-if ((lang === 'fr') && lastPart !== "fr") {
+if ((lang === 'fr') && (getCookie("first_visit") != "true") && lastPart !== "fr") {
+    document.cookie = "first_visit=true";
     window.location.href = "/fr";
+};
+
+// from http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
 };
